7ecdb5e84d2e99cfc2db36047b8e2a44c5ad9ba1
dededc37 <134894425+dededc37@users.noreply.github.com>
2025-01-28 12:53:56 -0300

    começando estudos sobre flask

---
+++ flask01/meu_site.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+#criar a 1ª página do site
+#route -> dede.com.br/alguma_página_com_sub_dominio
+#função -> oque você quer exibir ná página
+@app.route('/')
+def home():
+    return render_template('home/android.html')
+
+#colocar o site no ar
+if __name__== '__main__':
+    app.run(debug=True)
